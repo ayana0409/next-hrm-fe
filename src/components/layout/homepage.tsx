cbd3c67dbb4010d4a4c05d72021cd09ee6c67c38
ayana0409 <duongdoanthuan2003@gmail.com>
2025-10-01 13:48:21 +0700

    base code

---
+++ src/components/layout/homepage.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { CrownOutlined } from "@ant-design/icons"
+import { Result } from "antd"
+
+const HomePage = () => {
+    return (
+        <div style={{ padding: 20 }}>
+            <Result
+                icon={<CrownOutlined />}
+                title="HRM - createdBy Thuan"
+            />
+        </div>
+    )
+}
+
+export default HomePage;
+
