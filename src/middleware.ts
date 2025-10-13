41ea987645debd37b3c7da206e081eaa4e1acfea
ayana0409 <duongdoanthuan2003@gmail.com>
2025-10-13 16:18:41 +0700

    change protected url

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 export { auth as middleware } from "@/auth";
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/dashboard/:path*"],
+  // matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
