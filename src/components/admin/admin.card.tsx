095a59168a28ea37d70a14cf950a32163e140bc8
ayana0409 <duongdoanthuan2003@gmail.com>
2025-11-13 16:08:30 +0700

    add employee chart

--- src/components/admin/admin.card.tsx
+++ src/components/admin/admin.card.tsx
@@ -1,27 +1,74 @@
 "use client";
 
-import { Button, Card, Col, message, Row } from "antd";
+import { Card, Col, Row } from "antd";
+import PieChart from "../chart/pie.chart";
+import { useEffect, useRef, useState } from "react";
+import { useAxiosAuth } from "@/utils/customHook";
+import { useSession } from "next-auth/react";
 
 const AdminCard = () => {
-  const [messageApi, contextHolder] = message.useMessage();
+  const isMounted = useRef(false);
+  const { status } = useSession();
+  const authAxios = useAxiosAuth();
+  const [chartData, setChartData] = useState<{ value: number; name: string }[]>(
+    []
+  );
+  const fetchData = () => {
+    authAxios
+      .get("dashboard")
+      .then((response) => {
+        const { total, checkOut, halfDay, onboard, offEmp } =
+          response.data.data;
+        setChartData([
+          { value: onboard, name: "On time" },
+          { value: halfDay, name: "Half day" },
+          { value: checkOut, name: "Check out" },
+          { value: offEmp, name: "Off" },
+          {
+            value: total - checkOut - halfDay - onboard - offEmp,
+            name: "Remaining",
+          },
+        ]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      if (!isMounted.current) {
+        fetchData();
+        isMounted.current = true;
+      } else {
+        fetchData();
+      }
+    }
+  }, [status]);
+
   return (
     <>
-      {contextHolder}
-      <Button onClick={() => messageApi.error("Lỗi test!")}>Test Toast</Button>
       <Row gutter={16}>
         <Col span={8}>
-          <Card title="Card title" variant={"borderless"}>
-            Card content
+          <Card
+            title="Current Shift Status of Employees"
+            variant={"borderless"}
+          >
+            <PieChart
+              id="pie-chart"
+              subtext="Status summary for today's workforce participation"
+              data={chartData}
+            />
           </Card>
         </Col>
         <Col span={8}>
-          <Card title="Card title" variant={"borderless"}>
-            Card content
+          <Card title="Number" variant={"borderless"}>
+            Updating...
           </Card>
         </Col>
         <Col span={8}>
-          <Card title="Card title" variant={"borderless"}>
-            Card content
+          <Card title="Top over-time" variant={"borderless"}>
+            Updating...
           </Card>
         </Col>
       </Row>
